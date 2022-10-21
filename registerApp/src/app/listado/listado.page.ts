ab1e278646f48f2081ce9c0b1709fce4e606dbd6
FerHenn <102255890+FerHenn@users.noreply.github.com>
2022-10-21 16:46:45 -0300

    List of Alumnos
    List of alumnos in the page of qr-code

---
+++ registerApp/src/app/listado/listado.page.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.page.html',
+  styleUrls: ['./listado.page.scss'],
+})
+export class ListadoPage implements OnInit {
+
+
+  users:any;
+  nonusers: any;
+  constructor(private router: Router, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    const user = localStorage.getItem('User')
+    this.http.get('http://localhost:3000/users').subscribe(res =>{
+    this.users = res
+    console.log(res)
+
+    },error =>{
+      console.log(error)
+    })
+  }
+
+}
